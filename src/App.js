1d441bb7165d67f834df4c90f38484c9c138702f
stereaam <mihai.sterea@yahoo.com>
2022-12-13 11:28:26 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import Home from './pages/Home';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Contact from './pages/Contact';
 
@@ -9,10 +9,10 @@ function App() {
   return (
     <div>
       <Home />
-        <Routes>
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
 
     </div>
   );
